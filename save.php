54d5c239522cb0be7507a1161a089cbd8987aa81
Maroine <tamach2@gmail.com>
2020-03-25 15:32:26 +0100

    stmt2

--- save.php
+++ save.php
@@ -8,10 +8,10 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $db->query("SET NAMES utf8");
 
 
-$email = $_POST["email"];
-$name = $_POST["name"];
-$lastname = $_POST["lastname"];
-$password = $_POST["password"];
+$email = htmlspecialchars($_POST["email"]);
+$name = htmlspecialchars($_POST["name"]);
+$lastname = htmlspecialchars($_POST["lastname"]);
+$password = htmlspecialchars($_POST["password"]);
 
 $stmt = $pdo->prepare("INSERT INTO users VALUES(':email',':name',':lastname',':password')");
 $res = $stmt->execute([':email' => $email,':name' => $name,':lastname' => $lastname,':password' => $password]);
